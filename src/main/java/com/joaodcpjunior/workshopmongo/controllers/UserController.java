1c8343b99d47ad072042bba99f45699cd5655dd2
João Junior <joaodcpjunior@gmail.com>
2021-08-15 10:24:29 -0300

    Atualização de usuario com PUT

--- src/main/java/com/joaodcpjunior/workshopmongo/controllers/UserController.java
+++ src/main/java/com/joaodcpjunior/workshopmongo/controllers/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,7 +44,7 @@ public class UserController {
     public ResponseEntity<Void> insert(@RequestBody UserDto objDto) {
         User obj = service.fromDto(objDto);
         service.insert(obj);
-        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();//passando a localizaçãom do novo recurso criado pelo cabeçalho da resposta, boa prática
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();//passando a localização do novo recurso criado pelo cabeçalho da resposta, boa prática
 		return ResponseEntity.created(uri).build();
     }
 
@@ -51,6 +52,14 @@ public class UserController {
     public ResponseEntity<Void> delete(@PathVariable String id) {
         service.delete(id);
         return ResponseEntity.noContent().build();  
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Void> update(@RequestBody UserDto objDto, @PathVariable String id) {
+        User obj = service.fromDto(objDto);
+        obj.setId(id);
+        service.update(obj);
+		return ResponseEntity.noContent().build();
     }
 
 }
